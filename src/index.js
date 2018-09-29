ba4c24f0e0082f81e89c664d9034d5d7990a17c3
shet1990 <shet1990@gmail.com>
2018-09-30 00:23:38 +0300

    additional-5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let check;
+    let subStr;
+
+    while (true) {
+        check = false;
+        bracketsConfig.forEach(config => {
+            subStr = config.join('');
+            if (str.includes(subStr)) {
+                str = str.replace(subStr, '');
+                check = true;
+            }
+        });
+        if (!check) break;
+    }
+    return (str.length === 0) ? true : false;
 }
